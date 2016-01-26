93588c6bd645833604c0c69f031b4029d38d19da
Muhammad Jawaid Shamshad <mjawaid@gmail.com>
2016-01-26 19:07:36 +0500

    Initial commit

---
+++ FirstWindowsFormsApp/MyForm.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FirstWindowsFormsApp
+{
+    public class MyForm : Form
+    {
+        private TextBox textBox;
+        private Button button;
+        private Label label;
+
+        public MyForm()
+        {
+            Text = "My Window";
+
+            textBox = new TextBox();
+            textBox.Left = 25;
+            textBox.Top = 25;
+            textBox.Width = 200;
+            Controls.Add(textBox);
+
+            button = new Button();
+            button.Left = 25;
+            button.Top = 75;
+            button.Width = 200;
+            button.Text = "Click Me";
+            Controls.Add(button);
+
+            label = new Label();
+            label.Left = 25;
+            label.Top = 125;
+            label.Width = 200;
+            label.Text = "Label";
+            Controls.Add(label);
+        }   
+    }
+}
---
+++ FirstWindowsFormsApp/Start.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FirstWindowsFormsApp
+{
+    class Start
+    {
+        public static void Main()
+        {
+            MyForm form = new MyForm();
+            Application.Run(form);
+        }
+    }
+}
